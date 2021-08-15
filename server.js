622f5f47ef72a2eb11edf48cae28d0f4ee89df5e
Ali Al-hjouj <alisalamehhjouj@gmail.com>
2021-08-15 12:41:08 +0300

    fix

--- server.js
+++ server.js
@@ -7,14 +7,12 @@ const PORT = process.env.PORT;
 
 const movies = require('./controller/movies.contoller')
 const weather =require('./controller/wearher.controller');
-const Yelp = require('./controller/Yelp.controller');
 // first api weather data
 
 app.get('/weather', weather );
 
 app.get('/movies',movies );
 
-app.get('/yelp',Yelp );
 
 app.listen(PORT, () => {
   console.log(`this is my port numbers ${PORT}`);
